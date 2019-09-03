46712589e394f0de633f9d766cabc2d665e118e7
Troy <21197034+Troy2000@users.noreply.github.com>
2019-09-03 14:58:19 +0200

    Implemented move system

--- ts/States/Gameplay.ts
+++ ts/States/Gameplay.ts
@@ -3,12 +3,12 @@ import 'pixi';
 import 'phaser';
 
 import IGame from '../Fabrique/IGame';
-import Label from '../Objects/Label';
-import LabeledButton from '../Objects/LabeledButton';
+//import Label from '../Objects/Label';
+//import LabeledButton from '../Objects/LabeledButton';
 import SoundManager from '../Managers/SoundManager';
 
-import {Sounds, Constants, Images} from '../Data';
-import {Menu} from './';
+import {Sounds, /*Constants,*/ Images} from '../Data';
+//import {Menu} from './';
 
 export default class Gameplay extends Phaser.State {
     public static Name: string = 'gameplay';
@@ -17,13 +17,13 @@ export default class Gameplay extends Phaser.State {
     public name: string = Gameplay.Name;
     public game: IGame;
 
-    private background: Phaser.Image;
-    private floor: Phaser.Image;
+    private background: Phaser.TileSprite;
+    private floor: Phaser.TileSprite;
 
     private player: Phaser.Sprite;
 
-    private text: Label;
-    private backBtn: LabeledButton;
+    //private text: Label;
+    //private backBtn: LabeledButton;
 
     constructor() {
         super();
@@ -37,51 +37,59 @@ export default class Gameplay extends Phaser.State {
     }
 
     public preload(): void {
-        //this.game.load.atlasJSONHash('playerscene', 'assets/atlases/walk/playerscene.png', 'assets/atlases/walk/playerscene.json');
-        this.game.load.spritesheet('playerscene', 'assets/atlases/walk/playerscene.png', 480, 480);
+        this.game.load.spritesheet('playerWalk', 'assets/images/player/walk/playerWalk.png', 480, 480);
     }
 
     public create(): void {
         super.create();
-
         //Send a screen view to Google to track different states
         // this.game.analytics.google.sendScreenView(this.name);
 
-        this.background = this.game.add.image(0, 0, Images.BACKGROUND);
-        this.floor = this.game.add.image(0, 680, Images.FLOOR);
+        //this.background = this.game.add.image(0, 0, Images.BACKGROUND);
+        this.background = this.game.add.tileSprite(0, 0, this.world.bounds.width, this.game.cache.getImage(Images.BACKGROUND).height, Images.BACKGROUND);
+        this.floor = this.game.add.tileSprite(0, 680, this.world.bounds.width, this.game.cache.getImage(Images.FLOOR).height, Images.FLOOR);
+        //this.floor = this.game.add.image(0, 680, Images.FLOOR);
 
         // Sprite
-        this.player = this.game.add.sprite(0, 0, 'playerscene');
+        this.player = this.game.add.sprite(0, 0, 'playerWalk');
+
         this.player.scale.setTo(0.2, 0.2);
-        this.player.position.setTo(0, (this.floor.y - (this.player.height / 1.25)));
+        this.player.position.setTo(100, (this.floor.y - (this.player.height / 1.25)));
 
         // Animation
-        //this.player.animations.add('walk', Phaser.Animation.generateFrameNames('assets/images/player/walk/', 1, 12, '', 2), 10, true, false);
-        this.player.animations.add('walk');
-        this.player.animations.play('walk', 30, true);
+        this.player.animations.add('pl_walk');
+        this.player.animations.play('pl_walk', 30, true, false);
 
-        let textStyle: any = {font: 'bold ' + 30 * Constants.GAME_SCALE + 'px Arial', fill: '#FFFFFF'};
+        this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
 
-        this.text = new Label(this.game, 0, 0, 'time_to_play', textStyle);
+        //let textStyle: any = {font: 'bold ' + 30 * Constants.GAME_SCALE + 'px Arial', fill: '#FFFFFF'};
 
-        this.backBtn = new LabeledButton(this.game, 0, 0, 'back', textStyle, this.startMenu, this, 300, 100);
-        this.backBtn.setFrames('btn_blue', 'btn_blue', 'btn_blue_onpress', 'btn_blue');
+        //this.text = new Label(this.game, 0, 0, 'time_to_play', textStyle);
+
+        //this.backBtn = new LabeledButton(this.game, 0, 0, 'back', textStyle, this.startMenu, this, 300, 100);
+        //this.backBtn.setFrames('btn_blue', 'btn_blue', 'btn_blue_onpress', 'btn_blue');
 
         this.resize();
     }
 
     public update(): void {
         //console.log(this.player.animations.play('walk'));
-        // Auto move
-        this.player.x += 3;
-        if (this.player.x > 800)  {
-            this.player.x = -50;
-        }
+
+        // Move background
+        this.background.tilePosition.x -= 2;
+        this.floor.tilePosition.x -= 2;
+
+        // Move player
+        // this.player.x += 3;
+
+        // if (this.player.x > 800)  {
+        //     this.player.x = -50;
+        // }
     }
 
-    private startMenu(): void {
-        this.game.state.add(Menu.Name, Menu, true);
-    }
+    // private startMenu(): void {
+    //     this.game.state.add(Menu.Name, Menu, true);
+    // }
 
     /**
      * Called every time the rotation or game size has changed.
@@ -92,10 +100,10 @@ export default class Gameplay extends Phaser.State {
         this.background.height = this.game.height;
         this.floor.width = this.game.width;
 
-        this.text.alignIn(this.world.bounds, Phaser.CENTER);
-
-        this.backBtn.x = this.game.width / 2;
-        this.backBtn.y = this.text.y + this.text.height + this.backBtn.height;
+        // this.text.alignIn(this.world.bounds, Phaser.CENTER);
+        //
+        // this.backBtn.x = this.game.width / 2;
+        // this.backBtn.y = this.text.y + this.text.height + this.backBtn.height;
     }
 
     public shutdown(): void {
@@ -103,7 +111,7 @@ export default class Gameplay extends Phaser.State {
 
         this.background = null;
         this.floor = null;
-        this.text = null;
-        this.backBtn = null;
+        //this.text = null;
+        //this.backBtn = null;
     }
 }
